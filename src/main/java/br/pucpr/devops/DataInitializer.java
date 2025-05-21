3be852e72f80a1fec63817b9064113e01e832b44
Geucimar Briatore <geucimar@gmail.com>
2025-05-21 19:46:23 -0300

    Repository

---
+++ src/main/java/br/pucpr/devops/DataInitializer.java
@@ -0,0 +1,20 @@
+package br.pucpr.devops;
+
+import br.pucpr.devops.model.Pessoa;
+import br.pucpr.devops.repository.PessoaRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initDatabase(PessoaRepository repository) {
+        return args -> {
+            repository.save(new Pessoa(null, "Maria"));
+            repository.save(new Pessoa(null, "João"));
+            repository.save(new Pessoa(null, "Ana"));
+        };
+    }
+}
