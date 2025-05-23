eafb565648bd6ea1d1a5dcbb8732c0513c6f34e5
Geucimar Briatore <geucimar@gmail.com>
2025-05-23 19:15:22 -0300

    Calculadora

--- src/main/java/br/pucpr/devops/service/CalculadoraService.java
+++ src/main/java/br/pucpr/devops/service/CalculadoraService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class CalculadoraService {
 
     public int somar(int a, int b) {
-        return a + b;
+        return a * b;
     }
 
     public int subtrair(int a, int b) {
