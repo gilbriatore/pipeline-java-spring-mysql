4b5a8a85e99ce55dd988492437584fdf43a495e0
Geucimar Briatore <geucimar@gmail.com>
2025-05-26 17:40:14 -0300

    Docker

--- src/test/java/br/pucpr/devops/repository/PessoaRepositoryTest.java
+++ src/test/java/br/pucpr/devops/repository/PessoaRepositoryTest.java
@@ -3,14 +3,33 @@ package br.pucpr.devops.repository;
 import br.pucpr.devops.model.Pessoa;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@SpringBootTest
 public class PessoaRepositoryTest {
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
 
     @Autowired
     private PessoaRepository repository;
