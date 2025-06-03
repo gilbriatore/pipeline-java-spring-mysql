c99011cdfa0d4e1a8a0d4e84f337b43682c29f3d
Geucimar Briatore <geucimar@gmail.com>
2025-06-02 23:54:14 -0300

    Metrics

--- src/main/java/br/pucpr/devops/AzureApplication.java
+++ src/main/java/br/pucpr/devops/AzureApplication.java
@@ -1,6 +1,7 @@
 package br.pucpr.devops;
 
 import com.microsoft.applicationinsights.attach.ApplicationInsights;
+import com.microsoft.applicationinsights.connectionstring.ConnectionString;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class AzureApplication {
 
     public static void main(String[] args) {
         ApplicationInsights.attach();
+        ConnectionString.configure("InstrumentationKey=c4d73764-91fc-46b0-bf01-359883323bad;IngestionEndpoint=https://brazilsouth-0.in.applicationinsights.azure.com/");
         SpringApplication.run(AzureApplication.class, args);
     }
 
