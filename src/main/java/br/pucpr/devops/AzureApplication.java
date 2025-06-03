aad8ccd84ca20383c22a919d5132a1279ec4a293
Geucimar Briatore <geucimar@gmail.com>
2025-06-02 23:05:33 -0300

    Metrics

--- src/main/java/br/pucpr/devops/AzureApplication.java
+++ src/main/java/br/pucpr/devops/AzureApplication.java
@@ -1,5 +1,6 @@
 package br.pucpr.devops;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AzureApplication {
 
     public static void main(String[] args) {
+        ApplicationInsights.attach();
         SpringApplication.run(AzureApplication.class, args);
     }
 
